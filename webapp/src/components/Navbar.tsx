26f3a01f17de7ce9f1faed78126872e875e017e0
swunoo <swunhtetoo@gmail.com>
2024-05-06 00:38:36 +0900

    Update navbar style

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -4,20 +4,22 @@ type NavProps = {active: string}
 
 export default function Navbar({active}: NavProps){
 
-    const baseStyle = 'capitalize';
-    const activeStyle = ' underline';
-    const linkStyle = (link: string) => 
-        baseStyle + (link === active ? activeStyle : '') 
+    const baseStyle = 'uppercase text-sm w-20 py-1 text-center';
+    const activeStyle = ' bg-secondary text-white font-medium';
+    const linkStyle = (link: string, idx: number) => 
+        baseStyle 
+        + (idx === 0 ? ' rounded-l-lg' : ' rounded-r-lg  hover:bg-secondary hover:text-white') 
+        + (link === active ? activeStyle : ' shadow bg-white font-light') 
 
     return (
         <nav className="
             flex justify-between items-center
         ">
             <img className='h-8' src={logo} alt="Logo" />
-            <div className="flex gap-16">
+            <div className="flex">
                 {
                     ['tasks', 'projects'].map((l, idx) => (
-                        <a key={idx} href={'/' + l} className={linkStyle(l)}>{l}</a>
+                        <a key={idx} href={'/' + l} className={linkStyle(l, idx)}>{l}</a>
                     ))
                 }
             </div>
