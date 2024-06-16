646658fd20bd969c32deae6fd45850a03aa58eb2
swunoo <swunhtetoo@gmail.com>
2024-06-16 13:51:39 +0900

    OAuth2 with pure RestTemplate

--- spring-server/odaat/src/main/java/com/odaat/odaat/config/BacklogOAuth2Config.java
+++ spring-server/odaat/src/main/java/com/odaat/odaat/config/BacklogOAuth2Config.java
@@ -1,13 +1,17 @@
 package com.odaat.odaat.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
 
 
+@Data
+@Component
 public class BacklogOAuth2Config {
+    @Value("${backlog.app.root}")
+    private String appRoot;
+
     @Value("${backlog.oauth2.client-id}")
     private String clientId;
 
@@ -22,17 +26,4 @@ public class BacklogOAuth2Config {
 
     @Value("${backlog.oauth2.redirect-uri}")
     private String redirectUri;
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("external")
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .authorizationUri(authorizationUri)
-                .tokenUri(tokenUri)
-                .redirectUri(redirectUri)
-                .scope("read", "write")
-                .build();
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
 }
