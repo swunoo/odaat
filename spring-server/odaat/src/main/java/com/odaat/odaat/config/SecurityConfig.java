09bb2df80f94e6bb0dedc6514ed386018cf2b64c
swunoo <swunhtetoo@gmail.com>
2024-06-19 19:34:04 +0900

    Add auth0 to backend

--- spring-server/odaat/src/main/java/com/odaat/odaat/config/SecurityConfig.java
+++ spring-server/odaat/src/main/java/com/odaat/odaat/config/SecurityConfig.java
@@ -3,33 +3,50 @@ package com.odaat.odaat.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // TODO: replace with non-deprecated methods
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().disable()
-                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
-                .httpBasic().disable()
-                .formLogin().disable();
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/", "/index.html", "/static/**",
+                    "/*.ico", "/*.json", "/*.png", "/api/user").permitAll()
+                .anyRequest().authenticated()
+            )
+            .csrf((csrf) -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+            )
+            .addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class)
+            .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class)
+            .oauth2Login();
         return http.build();
     }
 
     @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
+    public RequestCache refererRequestCache() { 
+        return new HttpSessionRequestCache() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+                String referrer = request.getHeader("referer");
+                if (referrer == null) {
+                    referrer = request.getRequestURL().toString();
+                }
+                request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
+                    new SimpleSavedRequest(referrer));
+
             }
         };
     }
---
+++ spring-server/odaat/src/main/java/com/odaat/odaat/config/SecurityConfigLegacy.java
@@ -0,0 +1,36 @@
+package com.odaat.odaat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigLegacy {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // TODO: replace with non-deprecated methods
+        http
+                .csrf().disable()
+                .cors().disable()
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .httpBasic().disable()
+                .formLogin().disable();
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+}
